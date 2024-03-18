e508c7ba6bdac538d1a727c76a32d7d8e4c33ea0
Victor kamau <Victor Wangari>
2024-03-18 19:06:31 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Victor';
+const welcomeMessage =  `Welcome to Flatbook,  ${currentUser}!`;
+const excitedWelcomeMessage =  welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, V!`
